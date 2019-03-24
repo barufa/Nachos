50f8626b590c6f5399ff49cba87560fb05de7b8a
lucianoghione <ldghione@gmail.com>
2019-03-24 19:47:10 -0300

    Avanzando con la practica 1, ejercicios 12,13,14

--- code/threads/thread_test.cc
+++ code/threads/thread_test.cc
@@ -12,7 +12,13 @@
 
 
 #include "system.hh"
+#include "synch.hh"
 
+
+typedef struct arg_sem{
+    char * name;
+    Semaphore * f;
+}arg_sem;
 
 /// Loop 10 times, yielding the CPU to another ready thread each iteration.
 ///
@@ -34,6 +40,25 @@ SimpleThread(void *name_)
     printf("!!! Thread `%s` has finished\n", name);
 }
 
+void
+SimpleThreadSem(void *_args)
+{
+    char * name   = ((arg_sem *)_args)->name;
+    Semaphore * f = ((arg_sem *)_args)->f;
+    for (unsigned num = 0; num <= 10; num++) {
+        f->P();
+        DEBUG('s',"Thread `%s` has entered in the critical zone\n",name);
+        if(num==10){
+            printf("!!! Thread `%s` has finished\n", name);
+        }else{
+            printf("*** Thread `%s` is running: iteration %u\n", name, num);
+            currentThread->Yield();   
+        }
+        DEBUG('s',"Thread `%s` is leaving the critical zone\n",name);
+        f->V();
+    }
+}
+
 /// Set up a ping-pong between several threads.
 ///
 /// Do it by launching ten threads which call `SimpleThread`, and finally
@@ -42,13 +67,33 @@ void
 ThreadTest()
 {
     DEBUG('t', "Entering thread test\n");
+#ifdef SEMAPHORE_TEST
+    Semaphore *f = new Semaphore("SEMAPHORE_TEST",3);
+    for(char i = '2';i<='5';i++){
+      arg_sem * argumentos = new arg_sem;
+      argumentos->f = f;
+      argumentos->name = new char [64];
+      strncpy(argumentos->name, "_nd", 64);
+      argumentos->name[0]=i;
+      Thread *newThread = new Thread(argumentos->name);
+      newThread->Fork(SimpleThreadSem, (void *) argumentos);
+    }
+
+    arg_sem * argumentos = new arg_sem;
+    argumentos->f = f;
+    argumentos->name = new char [64];
+    strncpy(argumentos->name, "1st", 64);
+    SimpleThreadSem((void *) argumentos);
+
+#else
     for(char i = '2';i<='5';i++){
       char *name = new char [64];
-      strncpy(name, "2nd", 64);
+      strncpy(name, "_nd", 64);
       name[0]=i;
       Thread *newThread = new Thread(name);
       newThread->Fork(SimpleThread, (void *) name);
     }
 
     SimpleThread((void *) "1st");
+#endif
 }
